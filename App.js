314b0d930a7dcda9ec548d3fba0a2c9b89c601f6
Amir Ahangari <triplea1373@gmail.com>
2018-07-27 17:03:26 +0430

    Style added to FilmRow components

--- App.js
+++ App.js
@@ -23,9 +23,6 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
         <FilmRow 
           title={'Harvey'}
           year={1950}
@@ -38,18 +35,7 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    marginTop: 30,
   },
 });
--- src/films/components/FilmRow.js
+++ src/films/components/FilmRow.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 type Props= {
     title: string,
@@ -12,13 +12,50 @@ type Props= {
 export default class FilmRow extends Component<Props,void>{
     render(){
         return (
-            <View>
+            <TouchableOpacity style={styles.container}>
                 <Image
-                    style={{width: 50,height:50}} 
+                    style={styles.filmImage} 
                     source={{ uri: this.props.image }} />
-                <Text>{this.props.title}</Text>
-                <Text>{this.props.year}</Text>
-            </View>
+                <View style={styles.textContainer}>
+                    <Text style={styles.title}>{this.props.title}</Text>
+                    <Text style={styles.year}>{this.props.year}</Text>
+                </View>
+            </TouchableOpacity>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderColor: 'lightgray',
+        height: 80,        
+    },
+    filmImage: {
+        margin: 15,
+        width: 45,
+        height: 60,
+    },
+    textContainer: {
+        flex: 1,
+    },
+    title: {
+        fontFamily: 'Avenir Next',
+        fontWeight: '500',
+        fontSize: 16,
+        color: '#333',
+    },
+    year: {
+        fontFamily: 'Avenir Next',
+        fontWeight: '300',
+        fontSize: 14,
+        color: '#999',
+    },
+    disclosure: {
+        margin: 15,
+    }
+});
